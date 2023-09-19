305fff1ece3c414864d354076743136fd7cb1bd9
José Suárez <142860946+jisumov@users.noreply.github.com>
2023-09-18 20:23:29 -0500

    Update popsicles.cpp

--- sololearn/popsicles/popsicles.cpp
+++ sololearn/popsicles/popsicles.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 
     int siblings, popsicles;
-
     cin >> siblings >> popsicles;
+    /* Ask the amount of siblings and popsicles */
     if(popsicles%siblings==0) {
         /* Check if the amount of popsicles can be given away equally to the siblings */
         cout << "give away";
